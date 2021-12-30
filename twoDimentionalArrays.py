4fe38a3690327465a698be974ca30dbf1ae6d08b
Qian Chen <chen.qian.dan@gmail.com>
2021-12-30 21:23:03 +1100

    2 dimentional array: append

--- twoDimentionalArrays.py
+++ twoDimentionalArrays.py
@@ -5,13 +5,18 @@ twoDArray = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(twoDArray)
 
 
-# insertion
-# can not insert one element
-# can insert one column
-# can insert one row
+# # insertion
+# # can not insert one element
+# # can insert one column
+# # can insert one row
 
-newTwoDArray = np.insert(twoDArray, 0, [10, 11, 12], axis= 1) # 0: row, 1: column
-print(newTwoDArray)
+# newTwoDArray = np.insert(twoDArray, 0, [10, 11, 12], axis= 1) # 0: row, 1: column
+# print(newTwoDArray)
 
-newTwoDArray = np.insert(twoDArray, 0, [10, 11, 12], axis= 0) # 0: row, 1: column
+# newTwoDArray = np.insert(twoDArray, 0, [10, 11, 12], axis= 0) # 0: row, 1: column
+# print(newTwoDArray)
+
+
+# append
+newTwoDArray = np.append(twoDArray, [[1, 2, 3]], axis= 0)
 print(newTwoDArray)
