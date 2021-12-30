86b1569006324ccd0e3a5d0b100bb336dc8c69a6
Qian Chen <chen.qian.dan@gmail.com>
2021-12-30 14:25:41 +1100

    Array insertion

--- createArray.py
+++ createArray.py
@@ -6,7 +6,19 @@ arr2 = array('d', [1, 2, 3])
 print(arr1)
 print(arr2)
 
-# output:
-# array('i', [1, 2, 3, 4, 5, 6])
-# array('d', [1.0, 2.0, 3.0])
+"""
+output:
+array('i', [1, 2, 3, 4, 5, 6])
+array('d', [1.0, 2.0, 3.0])
+"""
 
+
+
+
+# insertion
+arr1.insert(len(arr1) - 1, 9) # when array is full, insertion will create a larger array
+print(arr1)
+"""
+output:
+array('i', [1, 2, 3, 4, 5, 9, 6])
+"""
