9c86e02fdd52020c97a7d07fb181826edae8ae3a
Qian Chen <chen.qian.dan@gmail.com>
2021-12-30 22:51:04 +1100

    List: access

--- list.py
+++ list.py
@@ -11,3 +11,16 @@ lst = [[1, 2, 3], [4, 5], 6]
 #         print(v) # cannot do this because 6 is not iterable
 print(lst)
 
+
+# access
+lst = ['Milk', 'Cheese', 'Butter']
+print(lst[0])
+# print(lst[3]) # IndexEror
+
+# in
+print('milk' in lst) # case sensitive
+
+# lower
+a = lst[0].lower()
+print(a)
+
