52c871d12562575ef88597cd3f2982855ef51c71
Qian Chen <chen.qian.dan@gmail.com>
2021-12-30 14:09:05 +1100

    Create arrays using array module

---
+++ createArray.py
@@ -0,0 +1,12 @@
+from array import *
+
+arr1 = array('i', [1, 2, 3, 4, 5, 6])
+arr2 = array('d', [1, 2, 3])
+
+print(arr1)
+print(arr2)
+
+# output:
+# array('i', [1, 2, 3, 4, 5, 6])
+# array('d', [1.0, 2.0, 3.0])
+
