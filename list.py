87f18c1f2bbc5fdee7ab752ffc823fc0ebb44e12
Qian Chen <chen.qian.dan@gmail.com>
2021-12-30 22:44:24 +1100

    List: create

---
+++ list.py
@@ -0,0 +1,13 @@
+from typing import List
+
+# create
+lst = []
+lst = List[int]
+lst = [1, 2, 3]
+lst = [None] * 10
+lst = [[1, 2, 3], [4, 5], 6] 
+# for row in lst:
+#     for v in row:
+#         print(v) # cannot do this because 6 is not iterable
+print(lst)
+
