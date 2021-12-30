9ff081dc8b88dbd311bd13d99b12a3550ddecffb
Qian Chen <chen.qian.dan@gmail.com>
2021-12-30 21:44:15 +1100

    2 dimentional array: traverse

--- twoDimentionalArrays.py
+++ twoDimentionalArrays.py
@@ -33,3 +33,12 @@ def accessElements(array, rowIndex, columnIndex):
         print(array[rowIndex][columnIndex])
 
 accessElements(twoDArray, 0, 0)
+
+
+
+# traverse
+twoDArray = np.array([[11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9]])
+
+for i in range(len(twoDArray)):
+    for j in range(len(twoDArray[0])):
+        print(twoDArray[i][j])
