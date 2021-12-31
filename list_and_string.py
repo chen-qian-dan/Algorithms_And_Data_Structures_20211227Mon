e4fe4601a7bc5bdfb4ee03c18f7f3173de7fbb84
Qian Chen <chen.qian.dan@gmail.com>
2021-12-31 11:22:20 +1100

    List: split()

---
+++ list_and_string.py
@@ -0,0 +1,14 @@
+from typing import List
+# string to list
+a: str = 'spam'
+b: List[str] = list(a)
+print(b)
+
+
+a: str = 'spam spam spam'
+b: List[str] = a.split()
+print(b)
+
+a: str = 'spam-spam1-spam2'
+b: List[str] = a.split('-')
+print(b)
