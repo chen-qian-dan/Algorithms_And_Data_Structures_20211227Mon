a33cda2d6fc5ffeed01e62fe03f67d0087f54678
Qian Chen <chen.qian.dan@gmail.com>
2021-12-31 11:27:40 +1100

    List: sort

--- list_and_string.py
+++ list_and_string.py
@@ -13,5 +13,19 @@ a: str = 'spam-spam1-spam2'
 b: List[str] = a.split('-')
 print(b)
 
-
+# join
 print('_'.join(b))
+
+
+# sort
+lst: List[int] = [2, 4, 3, 1, 5, 7]
+lst.sort()
+print(lst)
+lst.sort(reverse = True)
+print(lst)
+
+lst: List[int] = [2, 4, 3, 1, 5, 7]
+newlst = sorted(lst)
+print(newlst)
+newlst = sorted(lst, reverse=True)
+print(newlst)
