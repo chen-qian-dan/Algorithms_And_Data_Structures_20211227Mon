132b22e3dc4482bc82701bb2706223d7a603a50b
Qian Chen <chen.qian.dan@gmail.com>
2022-01-01 17:19:29 +1100

    Dictionary: search

--- dictionary.py
+++ dictionary.py
@@ -10,4 +10,39 @@ print(d.keys()) # output dict_keys([1, 2, 3])
 print(d.values()) # output dict_values(['a', 'b', 'c'])
 
 print(type(d.values())) # <class 'dict_values'>
-print(d['not exist key']) # KeyError: 'not exist key'
+# print(d['not exist key']) # KeyError: 'not exist key'
+
+
+# Update ---------------------------------
+d = {'name': 'Qian', 'age': 37}
+d['age'] = 27
+print(d['age'])
+
+# add ---------------------------------
+d['phone'] = '0404'
+print(d)
+
+
+# traverse ---------------------------------
+print("traverse ---------------------------------")
+for k in d.keys():
+    print(k, d[k])
+
+print("print d -----------------------------")
+print(d)
+
+print("in d: --------------------------------")
+for k, v in enumerate(d):
+    print(k, v)
+
+# search -------------------------------------
+print("search -------------------------------------")
+target = 9
+
+def searchDict(d, target):
+    for k in d:
+        if d[k] == target:
+            print("True")
+    print("False")
+
+searchDict(d, target)
