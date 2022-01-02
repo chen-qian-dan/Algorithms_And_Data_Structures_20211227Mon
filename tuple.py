263b537b0dc7ad7f1ce852ddbae175b7c78a0146
Qian Chen <chen.qian.dan@gmail.com>
2022-01-02 22:23:42 +1100

    Tuple: access

--- tuple.py
+++ tuple.py
@@ -17,3 +17,16 @@ print(type(tup))
 
 tup = tuple('abc') 
 print(tup) # output: ('a', 'b', 'c')
+
+
+print("\n access ----------------------------------")
+tup = (1, 2, 3, 4)
+print(tup[0])
+print(tup[-1])
+print(tup[0:2]) # output: (1, 2)
+
+print(tup[::-1]) # output: (4, 3, 2, 1)
+
+print(sorted(tup)) # [1, 2, 3, 4]
+
+print(min(tup))
