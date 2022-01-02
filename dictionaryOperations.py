1ab2a5d8cb9a9fb3e5d9f12afc8b1c61ce9ac2f3
Qian Chen <chen.qian.dan@gmail.com>
2022-01-02 19:36:28 +1100

    Dictionary: all()

--- dictionaryOperations.py
+++ dictionaryOperations.py
@@ -1,8 +1,17 @@
 
-print("in ----------------------------------")
+print("\n in ----------------------------------")
 d = {'name': 'Qian', 'age': 37}
 if 'name' in d: # in keys()
     print("True")
 
 for k, v in enumerate(d): # index, keys
     print(k, v)
+
+
+print("\n all ----------------------------------")
+d = {1: True, 0: False}
+print(all(d))
+
+d = {0: False, 1: False}
+print(all(d))
+
