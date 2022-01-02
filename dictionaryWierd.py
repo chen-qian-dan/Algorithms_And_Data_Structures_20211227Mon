aea2b860ac73b7025c6bc2bd41d2743cc643c1c3
Qian Chen <chen.qian.dan@gmail.com>
2022-01-02 20:01:26 +1100

    Dictionary: id()

--- dictionaryWierd.py
+++ dictionaryWierd.py
@@ -4,3 +4,11 @@ print(a[(1,2)])
 
 a = {(1,2):1,(2,3):2}
 print(a[1,2])
+
+
+rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
+id1 = id(rec)
+del rec
+rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
+id2 = id(rec)
+print(id1 == id2)
