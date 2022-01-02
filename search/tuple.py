653031939d9034692a1f01fa973f5a345573b183
Qian Chen <chen.qian.dan@gmail.com>
2022-01-02 20:21:55 +1100

    Tuple: create

--- search/tuple.py
+++ search/tuple.py
@@ -6,8 +6,14 @@ print(tup)
 
 print("tuple with one item ---------------------")
 # tuple with only one element, must with ,
-tup =('a') # a string
+tup = ('a') # a string
 print(type(tup))
 
-tup =('a',) # a tuple
+tup = ('a',) # a tuple
 print(type(tup))
+
+tup = tuple() # create an empty tuple
+print(type(tup))
+
+tup = tuple('abc') 
+print(tup) # output: ('a', 'b', 'c')
