e5cfb3e26a92219e9f8200c5645ae314c304ba5b
Qian Chen <chen.qian.dan@gmail.com>
2022-01-02 20:19:31 +1100

    Tuple: with one item

---
+++ search/tuple.py
@@ -0,0 +1,13 @@
+tup = (1, 2, 3)
+print(tup)
+
+
+
+
+print("tuple with one item ---------------------")
+# tuple with only one element, must with ,
+tup =('a') # a string
+print(type(tup))
+
+tup =('a',) # a tuple
+print(type(tup))
