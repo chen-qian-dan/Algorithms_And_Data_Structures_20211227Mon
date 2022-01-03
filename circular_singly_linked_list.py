63052409ee5713b556c0e31d756f8055bfe121dd
Qian Chen <chen.qian.dan@gmail.com>
2022-01-03 15:31:34 +1100

    Circular Singly Linked List: Create

--- circular_singly_linked_list.py
+++ circular_singly_linked_list.py
@@ -2,12 +2,12 @@
 
 
 class Node:
-    def __init(self, value):
+    def __init__(self, value):
         self.value = value
         self.next = None 
 
 class CircularSinglyLinkedList:
-    def __init__(self, value):
+    def __init__(self):
         self.head = None 
         self.tail = None 
 
@@ -19,3 +19,19 @@ class CircularSinglyLinkedList:
             if node == self.tail:
                 break 
             node = node.next
+
+
+    def create(self, value):
+        node: Node = Node(value)
+        node.next = node
+        self.head = node
+        self.tail = node 
+       
+        return "The list has been created"
+
+
+
+csll: CircularSinglyLinkedList = CircularSinglyLinkedList()
+csll.create(1)
+
+print([node.value for node in csll])
