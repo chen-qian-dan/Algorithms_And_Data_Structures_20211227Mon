de98c788ff221a9a6a64d48f5eeb2d14f7771cc7
Qian Chen <chen.qian.dan@gmail.com>
2022-01-03 17:48:08 +1100

    Circular Singly Linked List: delete a node

--- circular_singly_linked_list.py
+++ circular_singly_linked_list.py
@@ -1,6 +1,6 @@
 # Circular Singly Linked List
 
-from typing import Optional
+from typing import Counter, Optional
 
 class Node:
     def __init__(self, value):
@@ -11,6 +11,7 @@ class CircularSinglyLinkedList:
     def __init__(self):
         self.head = None 
         self.tail = None 
+        self.count: int = 0
 
 
     def __iter__(self):
@@ -27,12 +28,14 @@ class CircularSinglyLinkedList:
         node.next = node
         self.head = node
         self.tail = node 
+        self.count += 1
        
         return "The list has been created"
 
     
     def insert(self, value, location):
         nodeNew: Node = Node(value)
+        self.count += 1
         # if the list is empty
         if self.head is None:
             nodeNew.next = nodeNew 
@@ -95,6 +98,44 @@ class CircularSinglyLinkedList:
         return -1 
 
 
+
+    def deleteANode(self, location: int):
+        if location < -1 or location > self.count - 1:
+            print("location is out of range[0, {self.count}]")
+            return 
+        # delete the first node
+        if location == 0:
+            if self.head == self.tail:
+                self.head = None 
+                self.tail = None 
+
+            else:
+                self.head = self.head.next
+                self.tail.next = self.head 
+
+        # delete the last node
+        elif location == -1 or location == self.count - 1:
+            if self.head == self.tail:
+                self.head = None 
+                self.tail = None  
+            else:
+                node: Node = self.head
+                while node.next != self.tail:
+                    node = node.next
+                self.tail = node
+                self.tail.next = self.head 
+        # delete the middle node 
+        else:
+            index = 0
+            node: Node = self.head
+            while index < location - 1:
+                index += 1
+                node = node.next
+                
+            node.next = node.next.next
+
+
+
 csll: CircularSinglyLinkedList = CircularSinglyLinkedList()
 csll.insert('a', 0)
 csll.insert('b', -1)
@@ -104,4 +145,6 @@ csll.insert('d', 3)
 csll.insert('e', 5)
 print([node.value for node in csll])
 # csll.traverse()
-print(csll.search(1))
+# print(csll.search(1))
+csll.deleteANode(6)
+print([node.value for node in csll])
