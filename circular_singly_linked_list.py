a70b218a74821fa1de60699b04aeb2be290c672d
Qian Chen <chen.qian.dan@gmail.com>
2022-01-03 15:28:08 +1100

    Circular Singly Linked List: class

--- circular_singly_linked_list.py
+++ circular_singly_linked_list.py
@@ -1,1 +1,21 @@
 # Circular Singly Linked List
+
+
+class Node:
+    def __init(self, value):
+        self.value = value
+        self.next = None 
+
+class CircularSinglyLinkedList:
+    def __init__(self, value):
+        self.head = None 
+        self.tail = None 
+
+
+    def __iter__(self):
+        node: Node = self.head
+        while node:
+            yield node
+            if node == self.tail:
+                break 
+            node = node.next
