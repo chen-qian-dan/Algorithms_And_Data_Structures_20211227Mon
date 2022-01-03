af105f448c82b25e1de00aedc25c4b923db8c001
Qian Chen <chen.qian.dan@gmail.com>
2022-01-03 14:52:00 +1100

    Singly Linked List: delete a node

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -43,6 +43,44 @@ class SinglyLinkedList:
                 tmpNode.next = nodeNew
 
 
+    # delete a node
+    def deleteNode(self, location):
+        if self.head is None:
+            print("The list is empty")
+            return 
+            
+        if location == 0:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None 
+            else:
+                self.head = self.head.next
+
+        elif location == -1:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None 
+            else:
+                node: Node = self.head
+                
+                while node.next is not None and node.next != self.tail:
+                    node = node.next
+                
+                self.tail = node
+                node.next = None 
+        else:
+            node: Node = self.head
+            index: int = 0
+            while index < location - 1 and node.next:
+                index += 1
+                node = node.next
+            
+            if node.next is None:
+                print("The location is out of range")
+            else: 
+                node.next = node.next.next 
+
+
     # traverse
     def traverse(self):
         if not self.head:
@@ -82,14 +120,18 @@ singlyLinkedList: SinglyLinkedList = SinglyLinkedList()
 
 
 singlyLinkedList.insert(1, -1)
-singlyLinkedList.insert(2, -1)
-singlyLinkedList.insert(3, -1)
-singlyLinkedList.insert(4, 0)
+# singlyLinkedList.insert(2, -1)
+# singlyLinkedList.insert(3, -1)
+# singlyLinkedList.insert(4, 0)
 
 
 
 # singlyLinkedList.traverse()
-print(singlyLinkedList.search(20))
+# print(singlyLinkedList.search(20))
+
+print([node.value for node in singlyLinkedList])
+
+print(singlyLinkedList.deleteNode(-1))
 
 print([node.value for node in singlyLinkedList])
 
