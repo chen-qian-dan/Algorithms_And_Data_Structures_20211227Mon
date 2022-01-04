899190c5eb209fec4ecd64ef00f1b003b66547c3
Qian Chen <chen.qian.dan@gmail.com>
2022-01-04 20:40:39 +1100

    Doubly Linked List: insert

--- 05-linked_lists/doubly_linked_list.py
+++ 05-linked_lists/doubly_linked_list.py
@@ -12,6 +12,7 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None 
         self.tail = None 
+        self.count = 0
 
 
     def __iter__(self):
@@ -29,11 +30,55 @@ class DoublyLinkedList:
             node.prev = self.tail 
             self.tail.next = node 
             self.tail = node 
+        self.count += 1
 
+    def insert(self, value, location: int):
+        node: Node = Node(value)
+        if location < -1 or location > self.count:
+            print("location is out of range [0, {self.count}]")
+            return False 
+        # if list is empty
+        if not self.head:
+            self.head = node 
+            self.tail = node 
+        # insert at the beginning
+        elif location == 0:
+            node.next = self.head
+            self.head.pre = node 
+            self.head = node 
+        # insert at the end 
+        elif location == -1 or location == self.count:
+            node.pre = self.tail 
+            self.tail.next = node
+            self.tail = node 
+        # insert in the middle 
+        else:
+            index: int = 0
+            curNode: Node = self.head 
+            while index < location - 1:
+                curNode = curNode.next 
+                index += 1
+            node.next = curNode.next 
+            curNode.next.pre = node 
+            node.pre = curNode 
+            curNode.next = node 
+
+        self.count += 1 
+        return True 
+            
 
 
 
 dll: DoublyLinkedList = DoublyLinkedList()
 dll.append(1)
 dll.append(2)
+print([node.value for node in dll])
+
+
+# insert -----------------------------------------------
+dll: DoublyLinkedList = DoublyLinkedList()
+dll.append(1)
+print([node.value for node in dll])
+dll.insert(2, -1)
+dll.insert(3, -1)
 print([node.value for node in dll])
