e7f21834d70d5533631fcb203a0f5969fa237675
Qian Chen <chen.qian.dan@gmail.com>
2022-01-05 14:31:42 +1100

    Circular Doubly Linked List: insert

--- 05-linked_lists/circular_doubly_linked_list.py
+++ 05-linked_lists/circular_doubly_linked_list.py
@@ -11,7 +11,7 @@ class Node:
 class CircularDoublyLinkedList:
 
     def __init__(self):
-        self.count: int = -1
+        self.count: int = 0
         self.head = None 
         self.tail = None 
         
@@ -30,12 +30,65 @@ class CircularDoublyLinkedList:
         self.tail = node
         node.next = node 
         node.prev = node 
+        self.count += 1
         print("Create successfully")
 
 
+    def insert(self, value, location: int):
+        if self.head is None:
+            print("the list is not cated yet.")
+
+        if location < -1 or location > self.count + 1:
+            print(f"location is out of range [-1, {self.count}]")
+            return False 
+
+        node: Node = Node(value)
+        # insert at the beginning 
+        if location == 0:
+            node.next = self.head 
+            self.head.prev = node
+            self.head = node 
+
+            self.tail.next = node 
+            node.prev = self.tail 
+        # insert at the end 
+        elif location == -1 or location == self.count:
+            node.prev = self.tail 
+            self.tail.next = node
+            node.next = self.head 
+            self.head.prev = node 
+            self.tail = node 
+        
+        # insert in the middle 
+        else:
+            tmpNode: Node = self.head 
+            index = 0
+            while index < location - 1:
+                tmpNode = tmpNode.next
+                index += 1
+            node.next = tmpNode.next 
+            node.prev = tmpNode 
+            tmpNode.next.prev = node 
+            tmpNode.next = node 
+
+
+
+
 # create -------------------------------------
+# cdll: CircularDoublyLinkedList = CircularDoublyLinkedList()
+# cdll.create(1)
+# print([node.value for node in cdll])
+
+
+# insert -------------------------------------
 cdll: CircularDoublyLinkedList = CircularDoublyLinkedList()
 cdll.create(1)
+cdll.insert(0, location = -1)
+cdll.insert(2, location = 1)
+print([node.value for node in cdll])
+
+cdll.insert(3, location = 2)
+
 print([node.value for node in cdll])
 
 
