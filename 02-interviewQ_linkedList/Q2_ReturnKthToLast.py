a3656e8e2d288551c995da105fd76c451e61641f
Qian Chen <chen.qian.dan@gmail.com>
2022-01-06 23:30:49 +1100

    Q2: nthToLast

---
+++ 02-interviewQ_linkedList/Q2_ReturnKthToLast.py
@@ -0,0 +1,24 @@
+"""
+head -> node1 -> node2 -> node3 -> node4 
+2th to lask is node3
+"""
+from LinkedList import LinkedList, Node 
+
+def nthToLast(ll: LinkedList, n: int):
+    i: Node = ll.head 
+    j: Node = ll.head 
+    for k in range(n - 1):
+        j = j.next 
+    
+    while j != ll.tail:
+        i = i.next 
+        j = j.next 
+    return i 
+
+
+ll: LinkedList = LinkedList()
+ll.add(1)
+ll.add(2)
+ll.add(3)
+print(nthToLast(ll, 3).value)
+
