93264e3855aa08df1da969c5ed02c30aa439b0bf
Qian Chen <chen.qian.dan@gmail.com>
2022-01-06 22:57:17 +1100

    Q1: removeDuplicatesWithoutBuffer

--- 02-interviewQ_linkedList/Q1_RemoveDuplicates.py
+++ 02-interviewQ_linkedList/Q1_RemoveDuplicates.py
@@ -24,14 +24,48 @@ def removeDuplicates(ll):
         return  
 
 
+def removeDuplicatesWithoutBuffer(ll: LinkedList):
+    # list is empty
+    if not ll.head:
+        return 
+
+    # loop node i in range [0, n]
+    #   loop node j in range [i + 1, n]
+    #       if i.value == j.value:
+    #           {remove node j
+    #           if j == ll.tail                   
+    curNode: Node = ll.head 
+    
+    while curNode:
+        i = curNode
+        j = i.next
+        while j:
+            if curNode.value == j.value:
+                # remove j 
+                if j == ll.tail:
+                    ll.tail = i
+                    i.next = None 
+                else:
+                    i.next = j.next 
+
+                j = i.next
+            else:
+                i = i.next
+                j = i.next 
+        curNode = curNode.next
+
+    
+
+
 ll = LinkedList()
 ll.add(1)
-# ll.add(2)
-# ll.add(3)
-# ll.add(2)
-# ll.add(1)
+ll.add(2)
+ll.add(3)
+ll.add(2)
+ll.add(1)
 
 print(ll)
-removeDuplicates(ll)
+removeDuplicatesWithoutBuffer(ll)
+print(ll.tail.value)
 print(ll)
 
