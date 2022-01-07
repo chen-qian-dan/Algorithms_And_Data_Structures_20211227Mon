52bda2905b541d4968b091ac119c7b776028e2f2
Qian Chen <chen.qian.dan@gmail.com>
2022-01-08 00:37:15 +1100

    Stack with size limit: isEmpty, isFull

--- 06-Stack/stackWithLimitSize.py
+++ 06-Stack/stackWithLimitSize.py
@@ -15,6 +15,14 @@ class Stack:
         values = [str(x) for x in tmpList]
         return '\n'.join(values)
 
+
+    def isEmpty(self):
+        return True if len(self.list) == 0 else False 
+
+    def isFull(self):
+        return True if len(self.list) == self.maxSize else False 
+
+
     def push(self, value):
         if len(self.list) < self.maxSize:
             self.list.append(value)
@@ -24,9 +32,12 @@ class Stack:
 
 
 s = Stack(10)
+print(s.isEmpty())
 s.push(1)
 s.push(2)
 print(s)
 print("-------------")
 print(s)
+print(s.isEmpty())
+print(s.isFull())
 
