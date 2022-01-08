1ce0298cc0e93a8c372b481a2c9e6df47e755348
Qian Chen <chen.qian.dan@gmail.com>
2022-01-08 23:18:30 +1100

    circularQueue_usingPythonList_withSizeLimit: isFull

--- 07-Queue/circularQueue_usingPythonList_withSizeLimit.py
+++ 07-Queue/circularQueue_usingPythonList_withSizeLimit.py
@@ -1,8 +1,25 @@
 # circularQueue_usingPythonList_withSizeLimit
 
-class Queue:
+class CircularQueue:
     def __init__(self, maxSize):
         self.items = [None] * maxSize
         self.maxSize = maxSize 
         self.outIndex = -1
         self.inIndex = -1
+
+    def __str__(self):
+        values = [str(v) for v in self.items]
+        return ' '.join(values)
+
+    def isFull(self):
+        if self.inIndex == 0 and self.outIndex == self.maxSize - 1:
+            return True 
+        elif self.outIndex + 1 == self.inIndex:
+            return True 
+        else:
+            return False 
+
+
+
+cq = CircularQueue(6)
+print(cq.isFull())
