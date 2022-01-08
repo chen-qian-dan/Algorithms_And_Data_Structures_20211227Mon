0f06931d98379f9d6cf495fd78c580280149589a
Qian Chen <chen.qian.dan@gmail.com>
2022-01-08 21:26:05 +1100

    Stack created by Linked List: delete class LinkedList

--- 06-Stack/stackLinkedList.py
+++ 06-Stack/stackLinkedList.py
@@ -1,4 +1,5 @@
 
+# Stack created by Linked List
 
 class Node:
     def __init__(self, value):
@@ -6,38 +7,39 @@ class Node:
         self.next = None 
 
 
-class LinkedList:
+class Stack:
     def __init__(self):
         self.head = None 
-
+       
     def __iter__(self):
         node = self.head 
         while node:
             yield node 
             node = node.next 
-    
-    def __str__(self):
-        values = [str(x.value) for x in self]
-        return '->'.join(values)
-
-
-class Stack:
-    def __init__(self):
-        self.LinkedList = LinkedList()
 
     def __str__(self):
-        values = [str(x) for x in self.LinkedList] 
+        values = [str(x.value) for x in self] 
         return '->'.join(values)
 
 
     def isEmpty(self): # time: O(1), space: O(1)
-        return True if self.LinkedList.head is None else False 
+        return True if self.head is None else False 
 
     def push(self, value): # time: O(1), space: O(1)
         node = Node(value)
-        node.next = self.LinkedList.head 
-        self.LinkedList.head = node
+        node.next = self.head 
+        self.head = node
         print("Push successfully") 
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+             
+        value = self.head.value 
+        self.head = self.head.next 
+        return value 
+
+        
 
 
 
@@ -47,4 +49,7 @@ print(stack.isEmpty())
 stack.push(1)
 stack.push(2)
 
-print(stack.LinkedList)
+print(stack)
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
