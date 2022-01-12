f2473ac77abcafc6ed15119c4d669fc1b19f9d12
Qian Chen <chen.qian.dan@gmail.com>
2022-01-12 21:34:20 +1100

    Binary Search Tree Using Linked List:  insertNode

--- 08-Tree/BinarySearchTreeUsingLinkedList.py
+++ 08-Tree/BinarySearchTreeUsingLinkedList.py
@@ -10,4 +10,33 @@ class BSTNode:
         self.rightChild = None 
 
 
+def insertNode(root, data):
+    if not root:
+        root = BSTNode(data)
+    
+    if not root.data:
+        root.data = data
+        return "Insert successfully"
+    
+    newNode = BSTNode(data)
+
+    if data <= root.data:
+        if not root.leftChild:
+            root.leftChild = newNode 
+        else:
+            self.insertNode(root.leftChild, data)
+    else:
+        if not root.rightChild:
+            root.rightChild = newNode 
+        else:
+            self.insertNode(root.rightChild, data)
+
+    return "Insert successfully"
+
+
+
 newBTS = BSTNode(None)
+insertNode(newBTS, 70)
+insertNode(newBTS, 50)
+print(newBTS.data)
+print(newBTS.leftChild.data)
