c958323390065a0c7a1ba9f62fbdb13d85873b79
Qian Chen <chen.qian.dan@gmail.com>
2022-01-12 21:16:11 +1100

    Binary Search Tree Using Linked List: create

---
+++ 08-Tree/BinarySearchTreeUsingLinkedList.py
@@ -0,0 +1,13 @@
+# Binary Search Tree Using Linked List
+
+
+
+# create
+class BSTNode:
+    def __init__(self, data):
+        self.data = data 
+        self.leftChild = None 
+        self.rightChild = None 
+
+
+newBTS = BSTNode(None)
