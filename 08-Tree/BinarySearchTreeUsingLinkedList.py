88cd7c6d8f4c15ca5d23e2428d27232ac86f41fb
Qian Chen <chen.qian.dan@gmail.com>
2022-01-12 21:43:13 +1100

    Binary Search Tree Using Linked List: preOrderTraverse

--- 08-Tree/BinarySearchTreeUsingLinkedList.py
+++ 08-Tree/BinarySearchTreeUsingLinkedList.py
@@ -24,19 +24,40 @@ def insertNode(root, data):
         if not root.leftChild:
             root.leftChild = newNode 
         else:
-            self.insertNode(root.leftChild, data)
+            insertNode(root.leftChild, data)
     else:
         if not root.rightChild:
             root.rightChild = newNode 
         else:
-            self.insertNode(root.rightChild, data)
+            insertNode(root.rightChild, data)
 
     return "Insert successfully"
+
+
+def preOrderTraverse(rootNode: BSTNode):
+    if not rootNode:
+        return 
+        
+    print(rootNode.data)
+    if rootNode.leftChild:
+        preOrderTraverse(rootNode.leftChild)
+
+    if rootNode.rightChild:
+        preOrderTraverse(rootNode.rightChild)
 
 
 
 newBTS = BSTNode(None)
 insertNode(newBTS, 70)
 insertNode(newBTS, 50)
-print(newBTS.data)
-print(newBTS.leftChild.data)
+insertNode(newBTS, 90)
+insertNode(newBTS, 30)
+insertNode(newBTS, 60)
+insertNode(newBTS, 80)
+insertNode(newBTS, 100)
+insertNode(newBTS, 20)
+insertNode(newBTS, 40)
+# print(newBTS.data)
+# print(newBTS.leftChild.data)
+
+preOrderTraverse(newBTS)
