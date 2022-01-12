465580f568d1b681ad0e75edb85f7f2f38b3bdff
Qian Chen <chen.qian.dan@gmail.com>
2022-01-12 18:52:20 +1100

    Binary Tree using Python List: insertNode

--- 08-Tree/BinaryTreeUsingPythonList.py
+++ 08-Tree/BinaryTreeUsingPythonList.py
@@ -4,7 +4,21 @@ class BinaryTree:
     def __init__(self, size):
         self.list = [None] * size 
         self.maxSize = size 
-        self.lastUsedIndex = 0
+        self.lastUsedIndex: int = 0
+
+
+    # insert a node
+    # The binary tree is full 
+    # we have to look for a first vacant place 
+    def insertNode(self, value):
+        if self.lastUsedIndex == self.maxSize - 1:
+            return "The binary tree is full"
+        self.lastUsedIndex += 1
+        self.list[self.lastUsedIndex] = value 
+        return "The value has been successfully inserted"
 
 
 bt = BinaryTree(8)
+print(bt.insertNode("Drinks"))
+print(bt.insertNode("Hot"))
+print(bt.insertNode("Cold"))
