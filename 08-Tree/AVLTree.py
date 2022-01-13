352dc9b848966b25b6f010965e6d508c77733c54
Qian Chen <chen.qian.dan@gmail.com>
2022-01-13 22:11:35 +1100

    AVL Tree: create

---
+++ 08-Tree/AVLTree.py
@@ -0,0 +1,12 @@
+
+# AVL Tree
+
+class AVLNode:
+    def __init__(self, data):
+        self.data = data 
+        self.leftChild = None 
+        self.rightChild = None 
+        self.height = 1
+
+    
+avl = AVLNode(10)
