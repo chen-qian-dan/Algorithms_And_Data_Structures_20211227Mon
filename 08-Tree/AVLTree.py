6219b46294f34e1d898dd18da30469651c47efc2
Qian Chen <chen.qian.dan@gmail.com>
2022-01-13 22:41:00 +1100

    AVL Tree: levelOrderTraverse

--- 08-Tree/AVLTree.py
+++ 08-Tree/AVLTree.py
@@ -1,5 +1,45 @@
 
 # AVL Tree
+class QNode:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None 
+
+class Queue:
+    # enqueue at the end
+    # dequeue at the beginning
+    def __init__(self):
+        self.head = None 
+        self.tail = None 
+
+    def enqueue(self, data):
+        node = QNode(data)
+        if not self.head:
+            self.head = node 
+            self.tail = node 
+        else:
+            self.tail.next = node 
+            self.tail = node 
+
+    def dequeue(self):
+        if not self.head:
+            return "The queue is empty"
+        
+        data = self.head.data 
+        if self.head is self.tail:
+            self.head.data = None 
+            self.head = None 
+            self.tail = None 
+        else:
+            self.head = self.head.next 
+        return data 
+
+    def isEmpty(self):
+        return True if not self.head else False 
+
+
+
+
 
 class AVLNode:
     def __init__(self, data):
@@ -30,5 +70,22 @@ def postOrderTraverse(rootNode):
     postOrderTraverse(rootNode.rightChild)
     print(rootNode.data)
 
+def levelOrderTraverse(rootNode):
+    if not rootNode:
+        return 
+
+    q = Queue()
+    q.enqueue(rootNode)
+    while not q.isEmpty():
+        avlNode = q.dequeue()
+        print(avlNode.data)
+        if avlNode.leftChild:
+            q.enqueue(avlNode.leftChild)
+        if avlNode.rightChild:
+            q.enqueue(avlNode.rightChild)
+
+
     
 avl = AVLNode(10)
+
+levelOrderTraverse(avl)
