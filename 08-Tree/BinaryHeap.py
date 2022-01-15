82e5f11cc86b0a605dade0bd686b15584e0078ed
Qian Chen <chen.qian.dan@gmail.com>
2022-01-15 22:44:34 +1100

    Binary Heap: insertNode

--- 08-Tree/BinaryHeap.py
+++ 08-Tree/BinaryHeap.py
@@ -27,6 +27,42 @@ def levelTraverse(rootNode):
         print(rootNode.list[i])
 
 
+def heapifyTreeInsert(rootNode, index, heapType):
+    parentIndex = index // 2
+    if index <= 1:
+        return 
+    if heapType == "Min":
+        if rootNode.list[index] < rootNode.list[parentIndex]:
+            temp = rootNode.list[parentIndex]
+            rootNode.list[parentIndex] = rootNode.list[index]
+            rootNode.list[index] = temp 
+        heapifyTreeInsert(rootNode, parentIndex, heapType)
+    elif heapType == "Max":
+        if rootNode.list[index] > rootNode.list[parentIndex]:
+            temp = rootNode.list[parentIndex]
+            rootNode.list[parentIndex] = rootNode.list[index]
+            rootNode.list[index] = temp 
+        heapifyTreeInsert(rootNode, parentIndex, heapType)
+        
+
+def insertNode(rootNode, value, heapType):
+    if rootNode.heapSize + 1 == rootNode.maxSize:
+        return "The binary Heap is full"
+
+    rootNode.heapSize += 1
+    rootNode.list[rootNode.heapSize] = value 
+    heapifyTreeInsert(rootNode, rootNode.heapSize, heapType)
+    return "The value is inserted successfully"
+    
+
+
+
+
 heap = Heap(5)
 print(peekofHeap(heap))
 print(sizeofHeap(heap))
+insertNode(heap, 4, "Max")
+insertNode(heap, 5, "Max")
+insertNode(heap, 2, "Max")
+insertNode(heap, 1, "Max")
+levelTraverse(heap)
