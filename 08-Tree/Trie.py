2ded028e00381ab9a81473d27287429594e9c25c
Qian Chen <chen.qian.dan@gmail.com>
2022-01-16 00:33:24 +1100

    Trie: insertString

--- 08-Tree/Trie.py
+++ 08-Tree/Trie.py
@@ -11,4 +11,18 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
 
+
+    def insertString(self, word):
+        cur = self.root 
+        for ch in word:
+            if not ch in cur.children.keys():
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+        cur.endOfString = True 
+        print("Successfully inserted")
+
+
 trie = Trie()
+trie.insertString("App")
+trie.insertString("Appl")
+
