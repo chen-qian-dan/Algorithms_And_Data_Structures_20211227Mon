dcd4947afc2174b291b5151e40f2d4157d458ead
Qian Chen <chen.qian.dan@gmail.com>
2022-01-16 15:40:08 +1100

    Sorting: insertionSort

--- 09-Sorting/sortingAlgorithms.py
+++ 09-Sorting/sortingAlgorithms.py
@@ -1,6 +1,6 @@
 
 
-# Bubble sort
+# Sorting
 from typing import List 
 
 def bubbleSort(lst: List[int]): # by ref
@@ -18,9 +18,17 @@ def selectionSort(lst: List[int]):
                 minIndex = j
         lst[i], lst[minIndex] = lst[minIndex], lst[i]
 
+
+def insertionSort(lst: List[int]):
+    for i in range(len(lst)):
+        for j in range(i, 0, -1):
+            if lst[j] < lst[j - 1]:
+                lst[j], lst[j - 1] = lst[j - 1], lst[j]
+
 lst = [5, 4, 3, 2, 1] 
 # bubbleSort(lst)
-selectionSort(lst)
+# selectionSort(lst)
+insertionSort(lst)
 print(lst)
 
 
