46635b76715b24dcbbe0baa210e55147ad409132
Qian Chen <chen.qian.dan@gmail.com>
2022-01-16 15:59:59 +1100

    Sorting: bucketSort

--- 09-Sorting/sortingAlgorithms.py
+++ 09-Sorting/sortingAlgorithms.py
@@ -2,6 +2,7 @@
 
 # Sorting
 from typing import List 
+import math
 
 def bubbleSort(lst: List[int]): # by ref
     for j in range(len(lst) - 1, 0, -1):
@@ -25,10 +26,31 @@ def insertionSort(lst: List[int]):
             if lst[j] < lst[j - 1]:
                 lst[j], lst[j - 1] = lst[j - 1], lst[j]
 
+
+def bucketSort(lst: List[int]):
+    bucketCount = round(math.sqrt(len(lst)))
+    maxValue = max(lst)
+    buckets = list()
+    for i in range(bucketCount):
+        buckets.append(list())
+    for i in lst:
+        index = math.ceil(i * bucketCount / maxValue)
+        buckets[index - 1].append(i)
+    for l in buckets:
+        insertionSort(l)
+
+    retList = list()
+    for b in buckets:
+        retList.extend(b)
+    return retList
+
+
+
 lst = [5, 4, 3, 2, 1] 
 # bubbleSort(lst)
 # selectionSort(lst)
-insertionSort(lst)
+# insertionSort(lst)
+lst = bucketSort(lst)
 print(lst)
 
 
