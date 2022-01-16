12d749756fdff6728fc2d74a62ddf4f2912c6209
Qian Chen <chen.qian.dan@gmail.com>
2022-01-16 15:22:53 +1100

    Bubble sort: selectionSort

--- 09-Sorting/bubbleSort.py
+++ 09-Sorting/bubbleSort.py
@@ -8,11 +8,19 @@ def bubbleSort(lst: List[int]): # by ref
         for i in range(0, j):
             if lst[i] > lst[i + 1]:
                 lst[i], lst[i + 1] = lst[i + 1], lst[i]
-                
 
+
+def selectionSort(lst: List[int]):
+    for i in range(len(lst)):
+        minIndex = i
+        for j in range(i, len(lst)):
+            if lst[j] < lst[minIndex]:
+                minIndex = j
+        lst[i], lst[minIndex] = lst[minIndex], lst[i]
 
 lst = [5, 4, 3, 2, 1] 
-bubbleSort(lst)
+# bubbleSort(lst)
+selectionSort(lst)
 print(lst)
 
 
