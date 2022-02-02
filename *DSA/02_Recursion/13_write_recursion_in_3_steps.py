7012a7431a2cae94bafd9da8b0b1bad27e570c75
Qian Chen <chen.qian.dan@gmail.com>
2022-02-02 20:39:32 +1100

    Recursion: write recursion in 3 steps

---
+++ *DSA/02_Recursion/13_write_recursion_in_3_steps.py
@@ -0,0 +1,25 @@
+"""
+Factorial
+0! = 1
+4! = 4 * 3 * 2 * 1
+
+step 1: recursion case
+n! = n * (n - 1)!
+
+step 2: Base case - the stopping criterion 
+step 3: Unintentional case - the constraint
+factorial(-1) ?
+factorial(1.5) ?
+n >= 0
+n is int
+"""
+
+def factorial(n: int) -> int:
+    if not isinstance(n, int):
+        raise TypeError("n must be int")
+    if n < 0:
+        raise ValueError("n >= 0 must be True")
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+         
