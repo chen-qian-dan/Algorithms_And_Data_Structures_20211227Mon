01cd04725898fada409db876a50d3dfd99db9fe6
Qian Chen <chen.qian.dan@gmail.com>
2022-02-03 22:54:56 +1100

    List: 78_List operations and functions

---
+++ *DSA/08_List/78_List_operations_and_functions.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(c)
+
+
+a = [1, 2, 3]
+c = a * 2 
+print(c) # [1, 2, 3, 1, 2, 3]
+
+
+a = [1, 2, 3]
+print(len(a))
+
+print(max(a))
+print(min(a))
+
+print(sum(a))
