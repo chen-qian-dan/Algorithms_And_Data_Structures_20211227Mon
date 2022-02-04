34f0d685a0711e10b341cc89276ca64e4579bb59
Qian Chen <chen.qian.dan@gmail.com>
2022-02-04 21:00:46 +1100

    LeetCode: {15, "LeetCode 283"}

--- *DSA/LeetCode.py
+++ *DSA/LeetCode.py
@@ -1,2 +1,18 @@
+
+"""
+Algorithms:
+1. Brute Force
+2. Sliding window
+"""
+
+
+
 dsa = dict()
 dsa[0] =  ("10.2", "LeetCode 1")
+dsa[1] = ("10.5", "LeetCode 217")
+dsa[2] = ("10.7", "LeetCode 48")
+
+
+algorithms = dict()
+algorithms[0] = {15, "LeetCode 283"}
+
