afb9933b8422668aa20021db05220adfd614977e
Qian Chen <chen.qian.dan@gmail.com>
2022-02-18 00:32:14 +1100

    Binary Tree: preOrder_traverse

---
+++ *DSA/22_Tree/225_Binary_Tree.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.leftChild = None 
+        self.rightChild = None 
+
+
+def preOrder_traverse(node: Node): # time O(n), space O(n)
+    if node is None:
+        return [] 
+    ret = [node.val]
+    ret.extend(preOrder_traverse(node.leftChild)) # time O(n/2)
+    ret.extend(preOrder_traverse(node.rightChild)) # time O(n/2)
+    return ret 
+
+
+
+
+root = Node(1)
+root.leftChild = Node(2)
+root.rightChild = Node(3)
+print(preOrder_traverse(root))
+
