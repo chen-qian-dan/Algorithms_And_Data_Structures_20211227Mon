c48739c63040b2cc7306c7a2238128da5ad6c8ae
Qian Chen <chen.qian.dan@gmail.com>
2022-02-18 00:35:50 +1100

    Binary Tree: inOrder_traverse

--- *DSA/22_Tree/225_Binary_Tree.py
+++ *DSA/22_Tree/225_Binary_Tree.py
@@ -13,6 +13,14 @@ def preOrder_traverse(node: Node): # time O(n), space O(n)
     ret.extend(preOrder_traverse(node.rightChild)) # time O(n/2)
     return ret 
 
+def inOrder_traverse(node: Node): # time O(n), space O(n)
+    if node is None:
+        return []
+    ret = inOrder_traverse(node.leftChild) # time O(n/2)
+    ret.append(node.val)
+    ret.extend(inOrder_traverse(node.rightChild)) # time O(n/2)
+    return ret 
+
 
 
 
@@ -20,4 +28,5 @@ root = Node(1)
 root.leftChild = Node(2)
 root.rightChild = Node(3)
 print(preOrder_traverse(root))
+print(inOrder_traverse(root))
 
