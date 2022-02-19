ef9bbc6407231b2d34bbc637a7d3b0e7e5ebfe9f
Qian Chen <chen.qian.dan@gmail.com>
2022-02-19 20:43:28 +1100

    BST: insert node

---
+++ *DSA/23_Binary_Search_Tree/247_BST.py
@@ -0,0 +1,33 @@
+
+from collections import deque 
+
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.leftChild = None 
+        self.rightChild = None 
+
+def insertNode(root: Node, val): # time O(logN), space O(logN)
+    if root is None:
+        root = Node(val)
+    elif val <= root.val:
+        root.leftChild = insertNode(root.leftChild, val)
+    else:
+        root.rightChild = insertNode(root.rightChild, val)
+    return root 
+
+
+def preOrder_traverse(root: Node) -> list():
+    if root is None:
+        return []
+    ret = list()
+    ret.append(root.val)
+    ret.extend(preOrder_traverse(root.leftChild))
+    ret.extend(preOrder_traverse(root.rightChild))
+    return ret 
+
+
+
+root = Node(10)
+insertNode(root, 2)
+print(preOrder_traverse(root))
