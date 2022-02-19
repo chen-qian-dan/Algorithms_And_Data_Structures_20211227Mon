f48c652b7844c132f3bd6adc632fd1aea8c7000c
Qian Chen <chen.qian.dan@gmail.com>
2022-02-19 18:26:37 +1100

    Binary Tree: Python list: insert node

--- *DSA/22_Tree/234_Binary_Tree_use_Python_list.py
+++ *DSA/22_Tree/234_Binary_Tree_use_Python_list.py
@@ -13,6 +13,22 @@ class Binary_Tree:
     def __str__(self):
         return str(self.items)
 
+    def insertNode(self, val): # time O(1), space O(1)
+        """
+        1. the binary tree is full
+        2. find a first vacant place 
+        """
+        if self.lastUsedIndex == self.capacity - 1:
+            print("The tree is full")
+        self.lastUsedIndex += 1
+        self.items[self.lastUsedIndex] = val 
+
+
 
 tree = Binary_Tree(10)
+tree.insertNode(1)
+tree.insertNode(2)
+tree.insertNode(3)
+tree.insertNode(4)
+tree.insertNode(5)
 print(tree)
