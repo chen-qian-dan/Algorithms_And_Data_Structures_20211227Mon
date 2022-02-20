01b1f65e1336990baf67a5b4bc1917b15364fb2c
Qian Chen <chen.qian.dan@gmail.com>
2022-02-20 11:03:00 +1100

    Sorting: 301_bubble_sort.py

---
+++ *DSA/28_Sort_Algorithms/301_Bubble_Sort.py
@@ -0,0 +1,20 @@
+"""
+Bubble sort / Sinking sort 
+"""
+from typing import List 
+
+def bubble_sort(lst: List[int]): # by ref
+    if len(lst) <= 1:
+        return lst
+    for j in range(len(lst) - 1, -1, -1):
+        for i in range(0, j):
+            if lst[i] > lst[i + 1]:
+                lst[i], lst[i + 1] = lst[i + 1], lst[i]
+    return lst
+
+
+lst = [3, 5, 8, 1, 2, 9, 4, 7, 6]
+a = bubble_sort(lst)
+
+print(a)
+        
