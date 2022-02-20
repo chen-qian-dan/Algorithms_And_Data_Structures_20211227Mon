682cc233d4f583a79488f45cdeebd96c210f8535
Qian Chen <chen.qian.dan@gmail.com>
2022-02-20 11:13:45 +1100

    Sorting: bubble sort: bugfix

--- *DSA/28_Sort_Algorithms/301_Bubble_Sort.py
+++ *DSA/28_Sort_Algorithms/301_Bubble_Sort.py
@@ -5,7 +5,7 @@ from typing import List
 
 def bubble_sort(lst: List[int]): # by ref
     if len(lst) <= 1:
-        return lst
+        pass 
     for j in range(len(lst) - 1, -1, -1):
         for i in range(0, j):
             if lst[i] > lst[i + 1]:
@@ -14,6 +14,7 @@ def bubble_sort(lst: List[int]): # by ref
 
 
 lst = [3, 5, 8, 1, 2, 9, 4, 7, 6]
+lst = [3]
 a = bubble_sort(lst)
 
 print(a)
