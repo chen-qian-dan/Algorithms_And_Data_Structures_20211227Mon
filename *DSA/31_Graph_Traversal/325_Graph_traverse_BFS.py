6a0886d4a1489f97af9973f2daafac33085fd2a9
Qian Chen <chen.qian.dan@gmail.com>
2022-02-21 21:15:05 +1100

    Graph: BFS

--- *DSA/31_Graph_Traversal/325_Graph_traverse_BFS.py
+++ *DSA/31_Graph_Traversal/325_Graph_traverse_BFS.py
@@ -14,9 +14,9 @@ class Graph:
         self.gdict[vertex].add(edge)
 
 
-    def BFS(self, vertex):
+    def BFS(self, vertex): # time O(v + e) space O(v + e)
         if self.gdict is None:
-            return "The graph is empty"
+            return "The graph does not exist"
 
         visitedDict = Dict.fromkeys(self.gdict.keys(), False)
         q = deque()
@@ -34,6 +34,28 @@ class Graph:
         return ret 
 
 
+    def DFS(self, vertex): # time O(v + e)  space O(v + e)
+        if self.gdict is None:
+            return "The graph does not exist"
+        ret = list()
+        visitedDict = Dict.fromkeys(self.gdict.keys(), False)
+        stack = deque()
+        stack.append(vertex)
+        while len(stack):
+            node = stack.pop()
+            if visitedDict[node] is False:
+                ret.append(node)
+                visitedDict[node] = True 
+                for v in self.gdict[node]:
+                    if visitedDict[v] is False:
+                        stack.append(v)
+                    
+        return ret 
+
+    
+
+
+
 g = {"a": ("b", "c"), 
     "b": ("a", "d", "e"), 
     "c": ("a", "e"), 
@@ -45,3 +67,4 @@ g = {"a": ("b", "c"),
 graph = Graph(gdict = g)
 print(graph.gdict)
 print(graph.BFS("a"))
+print(graph.DFS("a"))
