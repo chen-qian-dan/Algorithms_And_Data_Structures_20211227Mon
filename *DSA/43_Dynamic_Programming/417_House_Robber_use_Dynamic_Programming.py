029c930db17926e74c49d128883e7cfae1c99e3d
Qian Chen <chen.qian.dan@gmail.com>
2022-02-25 19:59:12 +1100

    Dynamic programming: 417_House robber use dynamic programming

---
+++ *DSA/43_Dynamic_Programming/417_House_Robber_use_Dynamic_Programming.py
@@ -0,0 +1,25 @@
+
+def house_robber_top_down(houses, index, dp):
+    if index >= len(houses):
+        return 0
+    if index not in dp.keys():
+        sub1 = houses[index] + house_robber_top_down(houses, index + 2, dp)
+        sub2 = house_robber_top_down(houses, index + 1, dp)
+        dp[index] = max(sub1, sub2)
+    return dp[index]
+
+
+def house_robber_bottom_up(houses):
+    tb = [0] * len(houses)
+    tb[-1] = houses[-1]
+    tb[-2] = max(houses[-1], houses[-2])
+    for i in range(len(houses)-3, -1, -1):
+        tb[i] = max(houses[i] + tb[i+2], tb[i+1])
+
+    return tb[0]
+
+
+dp = dict()
+houses = [6, 7, 1, 30, 8, 2, 4] # expect 41
+print(house_robber_top_down(houses, 0, dp))
+print(house_robber_bottom_up(houses))
