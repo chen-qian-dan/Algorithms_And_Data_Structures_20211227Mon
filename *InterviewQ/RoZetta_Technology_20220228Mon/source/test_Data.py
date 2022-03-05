1b32985b6380930187d80c281afb4851f0d681c8
Qian Chen <chen.qian.dan@gmail.com>
2022-03-06 01:00:21 +1100

    RoZetta: test_Data.py: Test_generator

--- *InterviewQ/RoZetta_Technology_20220228Mon/source/test_Data.py
+++ *InterviewQ/RoZetta_Technology_20220228Mon/source/test_Data.py
@@ -1,4 +1,7 @@
 import unittest
+
+import os 
+import pathlib
 from Data import Data 
 
 class Test_init(unittest.TestCase):
@@ -13,6 +16,44 @@ class Test_init(unittest.TestCase):
     def test_arg_value(self):
         with self.assertRaises(ValueError):
             _ = Data(path = "", windowSize = 0) # <------ windowSize should > 0
+
+
+
+class Test_generator(unittest.TestCase):
+    def test_windowSize_is_3(self):
+        file_path = pathlib.Path(__file__).parent.absolute()
+        oData = Data(path = os.path.join(file_path, "./test_data/close_prices.csv"), windowSize = 3)
+        expected_lst = [None, 
+                    None,
+                    22.937,
+                    23.077,
+                    22.99,
+                    23.037,
+                    22.967,
+                    23.12,
+                    23.24,
+                    23.397,
+                    23.833,
+                    24.24,
+                    24.58,
+                    24.29,
+                    23.85,
+                    23.523,
+                    23.3,
+                    23.28,
+                    23.143,
+                    23.057,
+                    22.92]
+        for i in range(len(expected_lst)):
+            expected = expected_lst[i]
+            _, _, actual, _ = next(oData.gen)
+            if not actual is None:
+                actual = round(actual, 3)
+            self.assertAlmostEqual(expected, actual)
+
+
+
+
 
 
 
