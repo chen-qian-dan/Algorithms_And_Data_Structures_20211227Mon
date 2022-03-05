75d7c70e258eaf558539528fd866dd186b39d5c1
Qian Chen <chen.qian.dan@gmail.com>
2022-03-06 01:22:26 +1100

    RoZetta: update run.sh

--- *InterviewQ/RoZetta_Technology_20220228Mon/run.sh
+++ *InterviewQ/RoZetta_Technology_20220228Mon/run.sh
@@ -1,10 +1,11 @@
 
 # ReadMe:
 # run on terminal : % ./run.sh "Input/close_prices.csv" 3 9 "output.csv"
+# % ./run.sh '/Users/qianchen/Documents/QC21_03_DOING/github_20210115Fri/Algorithms_And_Data_Structures_20211227Mon/*InterviewQ/RoZetta_Technology_20220228Mon/Input/close_prices.csv' 3 9 "output.csv"
 
 
 
 
 #!/bin/sh
 CWD="$(pwd)"
-python3 build.py $1 $2 $3 $4
+python3 ./source/build.py $1 $2 $3 $4
