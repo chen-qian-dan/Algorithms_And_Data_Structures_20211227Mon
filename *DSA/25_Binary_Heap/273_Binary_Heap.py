a33f4680cfd62cc049f496cebcd33bb6ab2efad8
Qian Chen <chen.qian.dan@gmail.com>
2022-03-07 00:57:24 +1100

    Heap: peak

--- *DSA/25_Binary_Heap/273_Binary_Heap.py
+++ *DSA/25_Binary_Heap/273_Binary_Heap.py
@@ -4,6 +4,16 @@ class Heap:
         self.heapSize = 0 
         self.maxSize = size + 1
 
+def peak(root):
+    if not root:
+        return None 
+    else:
+        return root.lst[1]
+
+
 
 # Creation : time O(1), space O(N)
 heap = Heap(5)
+
+# peak : return List[1] time O(1), space O(1)
+
