e965172f72df8b4a0642ee41d099f13d050a785d
Qian Chen <chen.qian.dan@gmail.com>
2022-03-06 19:32:46 +1100

    Trie: insert a string to trie

--- *DSA/26_Trie/281_Trie.py
+++ *DSA/26_Trie/281_Trie.py
@@ -14,5 +14,22 @@ class Trie:
     def __init__(self):
         self.root = TrieNode() 
 
+    def insertString(self, word): # time O(m), space O(m) m is the len of word
+        cur = self.root
+        for c in word:
+            if not c in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfString = True 
+
 
 trie = Trie() # create Trie : time O(1), space O(1)
+
+
+# insert a string in a Trie 
+"""
+1. The trie is blank 
+2. new string's prefix is common to another strings prefix 
+"""
+trie.insertString("App")
+trie.insertString("Appi")
