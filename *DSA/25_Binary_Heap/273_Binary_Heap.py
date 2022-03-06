c0848e6e68035ad647ee2f5ed332e2723b39cde5
Qian Chen <chen.qian.dan@gmail.com>
2022-03-07 01:46:56 +1100

    Heap: extract a node

--- *DSA/25_Binary_Heap/273_Binary_Heap.py
+++ *DSA/25_Binary_Heap/273_Binary_Heap.py
@@ -59,6 +59,57 @@ def insertNode(root, value, heapType): # time O(logN), space O(logN)
     return "The value has been successfully inserted"
 
 
+# extract a node from a binary heap 
+# extract the root value
+# replace the root with the end node
+# heapify again 
+def heapifyExtract(root, index, heapType):  # time O(logN), space O(logN)
+    leftIndex = index * 2
+    rightIndex = index * 2 + 1
+    swapChild = 0
+    if root.heapSize < leftIndex: # no child
+        return None 
+    elif root.heapSize == leftIndex: # only have left Child
+        if heapType == "Min" and root.lst[index] > root.lst[leftIndex]:
+            root.lst[index], root.lst[leftIndex] = root.lst[leftIndex], root.lst[index]
+            heapifyExtract(root, leftIndex, heapType)
+        elif heapType == "Max" and root.lst[index] < root.lst[leftIndex]:
+            root.lst[index], root.lst[leftIndex] = root.lst[leftIndex], root.lst[index]
+            heapifyExtract(root, leftIndex, heapType)
+    else: # has two children 
+        if heapType == "Min":
+            if root.lst[leftIndex] < root.lst[rightIndex] and root.lst[index] > root.lst[leftIndex]:
+                root.lst[index], root.lst[leftIndex] = root.lst[leftIndex], root.lst[index]
+                heapifyExtract(root, leftIndex, heapType)
+            elif root.lst[leftIndex] > root.lst[rightIndex] and root.lst[index] > root.lst[rightIndex]:
+                root.lst[index], root.lst[rightIndex] = root.lst[rightIndex], root.lst[index]
+                heapifyExtract(root, rightIndex, heapType)
+        else:
+            if root.lst[leftIndex] > root.lst[rightIndex] and root.lst[index] < root.lst[leftIndex]:
+                root.lst[index], root.lst[leftIndex] = root.lst[leftIndex], root.lst[index]
+                heapifyExtract(root, leftIndex, heapType)
+            elif root.lst[leftIndex] < root.lst[rightIndex] and root.lst[index] < root.lst[rightIndex]:
+                root.lst[index], root.lst[rightIndex] = root.lst[rightIndex], root.lst[index]
+                heapifyExtract(root, rightIndex, heapType)
+
+def extractNode(root, heapType): # time O(logN), space O(logN)
+    if root.heapSize == 0:
+        return None 
+    else:
+        extractNode = root.lst[1]
+        root.lst[1] = root.lst[root.heapSize]
+        root.lst[root.heapSize] = None 
+        root.heapSize -= 1
+        heapifyExtract(root, 1, heapType)
+        return extractNode
+
+
+
+
+
+
+
+
 # Creation : time O(1), space O(N)
 heap = Heap(5)
 print(sizeOfHeap(heap))
@@ -68,6 +119,10 @@ insertNode(heap, 2, "Max")
 insertNode(heap, 5, "Max")
 insertNode(heap, 4, "Max")
 insertNode(heap, 1, "Max")
+print(levelOrderTraversal(heap))
+print(extractNode(heap, "Max"))
+print(levelOrderTraversal(heap))
+print(extractNode(heap, "Max"))
 print(levelOrderTraversal(heap))
 
 
