b428c79804eee2c60e6163cfd25cc9333db9ebe6
Qian Chen <chen.qian.dan@gmail.com>
2022-03-07 00:59:56 +1100

    Heap: size of heap

--- *DSA/25_Binary_Heap/273_Binary_Heap.py
+++ *DSA/25_Binary_Heap/273_Binary_Heap.py
@@ -4,6 +4,9 @@ class Heap:
         self.heapSize = 0 
         self.maxSize = size + 1
 
+
+
+# peak : return List[1] time O(1), space O(1)
 def peak(root):
     if not root:
         return None 
@@ -11,9 +14,17 @@ def peak(root):
         return root.lst[1]
 
 
+# size of binary heap
+# return number of filled cells time O(1), space O(1)
+def sizeOfHeap(root):
+    if not root:
+        return None 
+    else:
+        return root.heapSize
 
 # Creation : time O(1), space O(N)
 heap = Heap(5)
+print(sizeOfHeap(heap))
+print(peak(heap))
 
-# peak : return List[1] time O(1), space O(1)
 
